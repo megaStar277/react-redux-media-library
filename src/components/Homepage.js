9c7d1f932ff85e31773b998b64763653d684e6d0
solarspecter315 <ted563473@gmail.com>
2017-03-06 11:17:16 -0500

    Get basic routing set up

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const HomePage = () => (
+  <div className="jumbotron center">
+    <h1 className="lead">Welcome to Media Library</h1>
+    <div>
+      <Link to="library">
+        <button className="btn btn-lg btn-primary">Visit Library</button>
+        </Link>
+    </div>
+  </div>
+);
+
+export default HomePage;
---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+import Header from '../common/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
